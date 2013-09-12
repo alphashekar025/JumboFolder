513462713c7d096ca4b2d9e6cd054dca47cca733
Shekar N H <alphashekar025@gmail.com>
2013-09-11 20:55:34 -0400

    Added comments

--- jumboFolder.py
+++ jumboFolder.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+#
+# This python script is a command line utility for Windows systems to find 
+# large folders in a given directory
+
+ 
 import os
 
 
 def get_size(_dir):
+    """ 
+    Recursive traverse through the dir
+    and return the total sizes files found in MB
+
+    """
     total_size = 0
     _kb = 0.0009765625
     _mb = _kb * 0.0009765625
@@ -22,14 +33,14 @@ def get_size(_dir):
 
 #Walk through the root directory
 def walk_through(root_dir):
-    h = {}
+    h = {}                  # dictionary for the folders
     if os.path.isdir(root_dir):
         dirnames = os.listdir(root_dir)
         for dir in dirnames:
-            h.update(get_size( os.path.join( root_dir, dir)))
+            h.update(get_size(os.path.join(root_dir, dir)))
         for key, value in sorted(h.iteritems(),
-             key=lambda (k,v) : (v,k), reverse=True):
-                 print "%s ==> %s MB"    %    (key, h[key])
+                                 key=lambda (k, v): (v, k), reverse=True):  # descending order
+                print "%s ==> %s MB" % (key, h[key])
     else:
         print "No such file/Dir found !!"
 
